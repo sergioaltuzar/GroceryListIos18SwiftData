e8352d0f0678153749430934a573c8ca09b9bd12
sergio altuzar <sergio.altuzar@gmail.com>
2025-06-03 15:34:11 -0600

    Initial Commit

---
+++ Grocery List/Grocery_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_ListApp.swift
+//  Grocery List
+//
+//  Created by Sergio Altuzar on 03/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
